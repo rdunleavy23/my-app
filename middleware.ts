4febc22fad44fbe4112afcc63a70db5cbca0f0d3
rdunleavy23 <rdunleavy23@gmail.com>
2025-10-27 16:40:04 -0500

    fix: add unsafe-inline to CSP script-src to allow inline scripts

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,7 @@ export function middleware(request: NextRequest) {
 
   const csp = `
     default-src 'self';
-    script-src 'self' https://www.googletagmanager.com https://www.google-analytics.com;
+    script-src 'self' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com;
     style-src 'self' 'unsafe-inline';
     img-src 'self' data: blob: https:;
     font-src 'self' data:;
